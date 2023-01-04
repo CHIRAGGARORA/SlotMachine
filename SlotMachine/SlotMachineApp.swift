74e6b68d3349dc0f1872d38e36ad86eadbf82514
Chirag Arora <arora2002chirag@gmail.com>
2023-01-04 18:54:48 +0530

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by chirag arora on 04/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
